daaf07953106b3d5523cdc663492501c21d66556
AlexisChartier <alexischartier30130pse@gmail.com>
2025-11-19 22:44:57 +0100

    feat: initial commit for discovery service

---
+++ src/main/java/com/polytech/eventy_eureka_service/EventyEurekaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.polytech.eventy_eureka_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventyEurekaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventyEurekaServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/polytech/eventy_eureka_service/EventyEurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polytech.eventy_eureka_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventyEurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
