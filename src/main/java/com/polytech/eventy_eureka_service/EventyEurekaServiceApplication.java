8685049a6a67697de9ecbe777abbec0233bcded4
AlexisChartier <alexischartier30130pse@gmail.com>
2025-11-20 14:44:14 +0100

    add eureka

--- src/main/java/com/polytech/eventy_eureka_service/EventyEurekaServiceApplication.java
+++ src/main/java/com/polytech/eventy_eureka_service/EventyEurekaServiceApplication.java
@@ -2,8 +2,10 @@ package com.polytech.eventy_eureka_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EventyEurekaServiceApplication {
 
 	public static void main(String[] args) {
